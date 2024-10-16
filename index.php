2a77322e642ab4e446553869f6078216ac2f5fe8
markjasongalang <markjasongalang3@gmail.com>
2024-10-17 00:16:37 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    $title = "Find gigs for students <3";
+    $css_file_name = "index";
+    include './partials/header.php';
+?>
+
+<div class="container">
+    <h1>StudentGig is live now!</h1>
+</div>
+
+<?php
+    include './partials/footer.php';
+?>
---
+++ partials/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>StudentGig - <?php echo $title; ?></title>
+
+        <!-- External CSS -->
+        <link rel="stylesheet" href="./css/style.css">
+        <link rel="stylesheet" href="./css/<?php echo $css_file_name; ?>.css">
+
+        <!-- Remix Icon -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.2.0/remixicon.min.css" integrity="sha512-MqL4+Io386IOPMKKyplKII0pVW5e+kb+PI/I3N87G3fHIfrgNNsRpzIXEi+0MQC0sR9xZNqZqCYVcC61fL5+Vg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    </head>
+
+    <body>
+        <nav>
+            <a href="./">
+                <img class="website-logo" src="./images/studentgig-logo-cropped.png" alt="StudentGig logo">
+            </a>
+
+            <ul>
+                <li><a class="special-link" href="./post-a-gig"><i class="ri-edit-fill"></i> Post a Gig</a></li>
+                <li><a href="./login">Login</a></li>
+                <li><a href="./signup">Signup</a></li>
+            </ul>
+        </nav>
